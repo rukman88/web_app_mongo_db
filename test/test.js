767d437ccffcf738f3d6dd36f9e0bc28414b88f4
Rukman Madushka <zcz1762@autuni.ac.nz>
2020-04-30 22:48:27 +1200

    jason updated

--- test/test.js
+++ test/test.js
@@ -6,11 +6,28 @@ describe("Checking Database connection", function() {
 
     var url = "http://localhost:3000";
 
-    it("returns status 200", function(done) {
+    it( "returns status 200", function(done){
       request(url, function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
       });
-     });
+//     request(url)
+//     .on('response', function(response)
+//     {
+//         console.log(response.statusCode)
+//        // expect(response.statusCode).to.equal(200);
+//         //done();
+//     })
 
+//     .on("error", function(err){
+//         console.log("Problem reaching URL: ", err);
+//     })
+    
+//     //.pipe(url)
+//     .on("finish", function(response) {
+//     console.log("done");
+// })
+}
+);
+    
   });
